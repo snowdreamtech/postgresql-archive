d00ebaf793855b3ba7a61a703b3d95b2ac70b46d
snowdream <sn0wdr1am@qq.com>
2024-08-24 22:30:43 +0800

    feat: postgres 16.3

--- build.sh
+++ build.sh
@@ -7,16 +7,16 @@ GITHUB_PROJECT1=ghcr.io/snowdreamtech/postgresql
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
     -t ${DOCKER_HUB_PROJECT}:latest \
-    -t ${DOCKER_HUB_PROJECT}:14.12\
-    -t ${DOCKER_HUB_PROJECT}:14 \
+    -t ${DOCKER_HUB_PROJECT}:16.3\
+    -t ${DOCKER_HUB_PROJECT}:16 \
     -t ${GITHUB_PROJECT}:latest \
-    -t ${GITHUB_PROJECT}:14.12\
-    -t ${GITHUB_PROJECT}:14 \
+    -t ${GITHUB_PROJECT}:16.3\
+    -t ${GITHUB_PROJECT}:16 \
     -t ${DOCKER_HUB_PROJECT1}:latest \
-    -t ${DOCKER_HUB_PROJECT1}:14.12\
-    -t ${DOCKER_HUB_PROJECT1}:14 \
+    -t ${DOCKER_HUB_PROJECT1}:16.3\
+    -t ${DOCKER_HUB_PROJECT1}:16 \
     -t ${GITHUB_PROJECT1}:latest \
-    -t ${GITHUB_PROJECT1}:14.12\
-    -t ${GITHUB_PROJECT1}:14 \
+    -t ${GITHUB_PROJECT1}:16.3\
+    -t ${GITHUB_PROJECT1}:16 \
     . \
     --push
